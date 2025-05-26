0b1c45b53a7b4333c26c0baae446a17cf6b62275
Evan Berg <evan.m.berg@gmail.com>
2025-05-26 19:02:05 -0400

    added rest of associations so far into coach and student models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  has_many :appointments
+
   validates :first_name, :last_name, presence: :true
   validates :email, :phone_number, presence: :true, uniqueness: true
 end
